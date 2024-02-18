f4f009e83e673442d11adf5e959627a3fdc23d0f
Krystyna <krystyna.kulik@gmail.com>
2024-02-18 17:42:49 +0100

    add templates

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-from flask import Flask
+from flask import Flask, request, render_template
 from flask_restful import Resource, Api
 import cv2
-
+import requests
+import numpy as np
 
 app = Flask(__name__)
 api = Api(app)
@@ -15,6 +16,32 @@ hog = cv2.HOGDescriptor()
 hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
 
 
+@app.route('/index', methods=['GET', 'POST'])
+def PeopleCounterForm():
+    if request.method == 'GET':
+        return render_template('index.html')
+    else:
+        if 'link' in request.form:
+            link = requests.get(request.form['link'], stream=True).raw
+            arr = np.asarray(bytearray(link.read()), dtype="uint8")  # przekształaca stringa na tabele
+            img = cv2.imdecode(arr, cv2.IMREAD_COLOR)
+            boxes, weights = hog.detectMultiScale(img, winStride=(8, 8))
+        else:
+            boxes, weights = None
+
+        return render_template('index.html', link=len(boxes))
+
+
+class PeopleCounterUrl(Resource):
+    def get(self):
+        url = request.args.get('url')
+        url = requests.get(url, stream=True).raw
+        arr = np.asarray(bytearray(url.read()), dtype="uint8")
+        img = cv2.imdecode(arr, cv2.IMREAD_COLOR)
+        boxes, weights = hog.detectMultiScale(img, winStride=(8, 8))
+        return {'count': len(boxes)}
+
+
 class PeopleCounter(Resource):
     def get(self):
         img = cv2.imread('images/dworzec.jpeg')
@@ -24,7 +51,7 @@ class PeopleCounter(Resource):
         # print(type(img))
         # print(type(boxes))  # lista
         # print(img.shape)
-        return {'count test': len(boxes)}
+        return {'Ilość osób na zdjęciu': len(boxes)}
 
 
 class HelloWorld(Resource):
@@ -40,6 +67,7 @@ class HelloWorld2(Resource):
 api.add_resource(HelloWorld, '/test')
 api.add_resource(HelloWorld2, '/test2')
 api.add_resource(PeopleCounter, '/')
+api.add_resource(PeopleCounterUrl, '/url')
 
 
 if __name__ == '__main__':
